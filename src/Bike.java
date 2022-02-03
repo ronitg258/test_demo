7123a56e0dd35553660e30c0933e357a44656d57
ronitg258 <98870503+ronitg258@users.noreply.github.com>
2022-02-03 12:43:46 +0530

    Add files via upload

---
+++ src/Bike.java
@@ -0,0 +1,18 @@
+package io.sevenx.vehiclemanagement;
+
+public class Bike extends Vehicle{
+	private int numberOfSeats;
+	private int numberOfWheels;
+	public int getNumberOfSeats() {
+		return numberOfSeats;
+	}
+	public void setNumberOfSeats(int numberOfSeats) {
+		this.numberOfSeats = numberOfSeats;
+	}
+	public int getNumberOfWheels() {
+		return numberOfWheels;
+	}
+	public void setNumberOfWheels(int numberOfWheels) {
+		this.numberOfWheels = numberOfWheels;
+	}
+}
