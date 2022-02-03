db3195739f78ae785391933a2c252e3a9f6283d5
ronitg258 <98870503+ronitg258@users.noreply.github.com>
2022-02-03 12:32:58 +0530

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,13 @@
+package io.sevenx.vehiclemanagement;
+
+public class Vehicle {
+	public void start(String vehicle) {
+		System.out.println(vehicle+" Engine Starts");
+	}
+	public void stop(String vehicle) {
+		System.out.println(vehicle+" Engine Stops");
+	}
+	public void typeOfVehicle(String vehicle) {
+		System.out.println(vehicle+" Is A Geared Vehicle");
+	}
+}
