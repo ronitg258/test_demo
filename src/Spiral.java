8857b6934ef20af516bfecc943b9d00a4702b842
ronitg258 <98870503+ronitg258@users.noreply.github.com>
2022-02-07 12:42:18 +0530

    Add files via upload

---
+++ src/Spiral.java
@@ -0,0 +1,37 @@
+// Spiral Problem
+import java.util.Scanner;
+public class Spiral {
+	public static void main(String args[]) {
+		int n, top, left, right, bottom, i = 0, k = 1;
+		int a[][];
+		
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		top = 0; left = 0; right = n - 1; bottom = n - 1;
+		a = new int[n][n];
+		while(top <= bottom && left <= right) {
+			for(i = left; i <= right; i++) {
+				a[top][i] = k++;
+			}
+			top++;
+			for(i = top; i <= bottom; i++) {
+				a[i][right] = k++;
+			}
+			right--;
+			for(i = right; i >= left; i--) {
+				a[bottom][i] = k++;
+			}
+			bottom--;
+			for(i = bottom; i >= top; i--) {
+				a[i][left] = k++;
+			}
+			left++;   
+		}
+		for(i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++){
+				System.out.print(a[i][j] + "\t");
+			}
+			System.out.println();
+		}
+	}
+}
